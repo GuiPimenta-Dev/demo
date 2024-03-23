500bc435f7aa7940966886350e655ce7021e237d
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-22 21:32:06 -0300

    fixing integration tests

--- functions/private_hello_world/integration.py
+++ functions/private_hello_world/integration.py
@@ -5,6 +5,9 @@ from lambda_forge.constants import BASE_URL
 @pytest.mark.integration(method="GET", endpoint="/private_hello_world")
 def test_private_hello_world_status_code_is_200():
 
-    response = requests.get(url=f"{BASE_URL}/private_hello_world")
+    headers = {
+        "secret": "9cgKnXvck1Lbh4N5ZaJcsqK0xL6DyRDzxjXmQ6VVQIOoooDF839z"
+    }
+    response = requests.get(url=f"{BASE_URL}/private_hello_world", headers=headers)
 
     assert response.status_code == 200 
